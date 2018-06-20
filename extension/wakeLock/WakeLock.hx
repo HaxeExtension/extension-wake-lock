3cc6db9ad3ecceb1b500123725b54bafe0eafeab
Will Blanton <will.blanton.804@gmail.com>
2018-06-20 16:38:35 -0400

    Updating JNI references

--- extension/wakeLock/WakeLock.hx
+++ extension/wakeLock/WakeLock.hx
@@ -1,11 +1,17 @@
 package extension.wakeLock;
 
+#if (openfl < "4.0.0")
+import openfl.utils.JNI;
+#else
+import lime.system.JNI;
+#end
+
 class WakeLock{
 	
 	public static var allowScreenTimeout(default, set):Bool=true;	
 	#if android
-	public static var setKeepScreenOn:Void->Void = openfl.utils.JNI.createStaticMethod("wakelock/AndroidWakeLock", "SetKeepScreenOn", "()V");
-	public static var setKeepScreenAuto:Void->Void = openfl.utils.JNI.createStaticMethod("wakelock/AndroidWakeLock", "SetKeepScreenAuto", "()V");
+	public static var setKeepScreenOn:Void->Void = JNI.createStaticMethod("wakelock/AndroidWakeLock", "SetKeepScreenOn", "()V");
+	public static var setKeepScreenAuto:Void->Void = JNI.createStaticMethod("wakelock/AndroidWakeLock", "SetKeepScreenAuto", "()V");
 	#else
 	public static var setKeepScreenOn:Void->Void= function(){};
 	public static var setKeepScreenAuto:Void->Void= function(){};
@@ -18,4 +24,3 @@ class WakeLock{
 		return true;
 	}
 }
-
